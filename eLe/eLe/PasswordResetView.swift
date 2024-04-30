9a2ee3e6ef1e88d0d6b4a19c0decd6f27b1dd0fb
Jean Paul Casique Hernandez <128648632+jeancasique@users.noreply.github.com>
2024-04-30 14:04:14 +0200

    commit martes

--- eLe/eLe/PasswordResetView.swift
+++ eLe/eLe/PasswordResetView.swift
@@ -1,44 +1,61 @@
 import SwiftUI
 import Firebase
+import UIKit
 
+// Definición de la vista para restablecer la contraseña
 struct PasswordResetView: View {
+    
+    // Estado para almacenar el correo electrónico ingresado por el usuario
     @State private var email = ""
+    
+    // Estado para controlar la visibilidad de la alerta
     @State private var showAlert = false
+    
+    // Estado para almacenar el mensaje de la alerta
     @State private var alertMessage = ""
+    
+    // Estado para validar si el correo electrónico ingresado es válido
     @State private var isEmailValid = true
 
     var body: some View {
         VStack {
+            Image("lock") // Usa el nombre del asset de la imagen
+                .resizable() // Hace que la imagen sea redimensionable
+                .scaledToFit() // Asegura que la imagen se escala proporcionalmente
+                .frame(width: 120, height: 120) // Establece un tamaño específico para la imagen
+                .padding(.top, 50) // Agrega un espacio en la parte superior
+
             TextField("Correo Electrónico", text: $email)
                 .padding()
-                .autocapitalization(.none) // evita que todo este en mayuscula
-                .keyboardType(.emailAddress) // hace que el teclado muestre el @ y el .com
-                .disableAutocorrection(true)// elimina el autocorrector
-                .border(Color(UIColor.separator)) // le da borde por sistema
+                .autocapitalization(.none) // Desactiva la autocapitalización del texto
+                .keyboardType(.emailAddress) // Define el tipo de teclado como dirección de correo electrónico
+                .disableAutocorrection(true) // Desactiva la autocorrección del texto
+                .border(Color(UIColor.separator)) // Agrega un borde alrededor del campo de texto
                 .padding(.horizontal, 8)
                 .padding(.vertical, 20)
-                .onChange(of: email, perform: validateEmail) // si hay algun cambio en el email aplica el metodo validateEmail
+                .onChange(of: email, perform: validateEmail) // Realiza la validación del correo electrónico cuando cambia
 
+            // Muestra un mensaje de error si el correo electrónico no es válido
             if !isEmailValid {
                 Text("Correo electrónico no válido")
                     .foregroundColor(.red)
                     .font(.caption)
             }
 
+            // Botón para restablecer la contraseña
             Button("Restablecer Contraseña") {
-                resetPassword()
+                resetPassword() // Llama a la función para restablecer la contraseña
             }
             .padding()
             .foregroundColor(.white)
             .background(Color.blue)
             .cornerRadius(8)
             .padding()
-
         }
         .padding()
-        .navigationTitle("Restablecer Contraseña")
-        .navigationBarTitleDisplayMode(.inline)
-        .alert(isPresented: $showAlert) {
+        .navigationTitle("Restablecer Contraseña") // Título de la vista en la barra de navegación
+        .navigationBarTitleDisplayMode(.inline) // Define el modo de visualización del título
+        .alert(isPresented: $showAlert) { // Muestra una alerta cuando showAlert es true
             Alert(
                 title: Text("Recuperar Contraseña"),
                 message: Text(alertMessage),
@@ -47,28 +64,31 @@ struct PasswordResetView: View {
         }
     }
 
+    // Función para restablecer la contraseña
     private func resetPassword() {
-        if isEmailValid {
-            Auth.auth().sendPasswordReset(withEmail: email) { error in
-                if let error = error {
+        if isEmailValid { // Verifica si el correo electrónico es válido
+            Auth.auth().sendPasswordReset(withEmail: email) { error in // Envía una solicitud para restablecer la contraseña
+                if let error = error { // Si hay un error al enviar la solicitud
                     alertMessage = "Error al enviar el correo electrónico de restablecimiento de contraseña: \(error.localizedDescription)"
-                } else {
+                } else { // Si la solicitud se envía correctamente
                     alertMessage = "Se ha enviado un correo electrónico de restablecimiento de contraseña a \(email). Por favor, verifica tu bandeja de entrada."
-                    email = "" // Limpiar el campo de correo electrónico después de enviar
+                    email = "" // Limpia el campo de correo electrónico después de enviar
                 }
-                showAlert = true
+                showAlert = true // Muestra la alerta
             }
         }
     }
 
+    // Función para validar el formato del correo electrónico
     private func validateEmail(_ email: String) {
-        isEmailValid = isValidEmail(email)
+        isEmailValid = isValidEmail(email) // Llama a la función isValidEmail para validar el correo electrónico
     }
 
+    // Función para validar el formato del correo electrónico utilizando una expresión regular
     private func isValidEmail(_ email: String) -> Bool {
         let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailFormat)
-        return emailPredicate.evaluate(with: email)
+        return emailPredicate.evaluate(with: email) // Retorna true si el correo electrónico coincide con el formato especificado
     }
 }
 
--- eLe/eLe/PerfilView.swift
+++ eLe/eLe/PerfilView.swift
@@ -3,7 +3,16 @@ import Firebase
 import FirebaseAuth
 import FirebaseFirestore
 import FirebaseStorage
-import GoogleSignIn
+
+// Define un tipo de datos que se conforme a Identifiable y encapsule UIImagePickerController.SourceType
+struct ImagePickerSourceType: Identifiable {
+    let id: UIImagePickerController.SourceType
+    
+    // Inicializador para asignar el id
+    init(id: UIImagePickerController.SourceType) {
+        self.id = id
+    }
+}
 
 // Clase para gestionar los datos del usuario utilizando el patrón ObservableObject
 class UserData: ObservableObject {
@@ -20,9 +29,9 @@ class UserData: ObservableObject {
 struct PerfilView: View {
     @StateObject private var userData = UserData() // Datos del usuario como objeto de estado
     @State private var editingField: String?       // Campo actualmente en edición
-    @State private var showImagePicker = false     // Controla la visibilidad del selector de imagen
     @State private var showAlert = false           // Controla la visibilidad de la alerta
     @State private var alertMessage = ""           // Mensaje para la alerta
+    @State private var sourceType: ImagePickerSourceType?
 
     var body: some View {
         NavigationView {
@@ -51,25 +60,20 @@ struct PerfilView: View {
                     }
                     .padding(.vertical) // Añade relleno vertical al contenedor HStack
 
-                    userInfoField(label: "Nombre", value: $userData.firstName, editing: $editingField, fieldKey: "firstName", editable: true)
-                    userInfoField(label: "Apellidos", value: $userData.lastName, editing: $editingField, fieldKey: "lastName", editable: true)
-                    datePickerField(label: "Fecha de Nacimiento", date: $userData.birthDate, editing: $editingField, fieldKey: "birthDate")
-                    userInfoField(label: "Género", value: $userData.gender, editing: $editingField, fieldKey: "gender", editable: true)
+                    // Otras secciones para mostrar y editar la información del usuario
+                    // ...
 
-                    // Botón para guardar los cambios realizados en el perfil del usuario
-                    Button("Guardar Cambios", action: saveData) // Define el botón y su acción
-                        .padding() // Añade relleno alrededor del botón
-                        .foregroundColor(.white) // Establece el color del texto a blanco
-                        .background(Color.blue) // Establece el color de fondo a azul
-                        .cornerRadius(10) // Redondea las esquinas del botón
+                    // Botón para seleccionar la imagen
+                    Button("Seleccionar Imagen") {
+                        sourceType = ImagePickerSourceType(id: .camera) // O .photoLibrary según corresponda
+                    }
 
                     Spacer() // Inserta un espacio flexible que empuja el contenido hacia arriba
                 }
                 .padding() // Añade relleno alrededor del VStack
             }
-            .onAppear(perform: loadUserData) // Carga los datos del usuario al aparecer la vista
-            .sheet(isPresented: $showImagePicker) { // Muestra un selector de imágenes cuando es necesario
-                ImagePicker(image: $userData.profileImage)
+            .sheet(item: $sourceType) { selectedSourceType in
+                ImagePicker(image: $userData.profileImage, sourceType: selectedSourceType.id)
             }
             .alert(isPresented: $showAlert) { // Muestra una alerta cuando se guarda la información
                 Alert(
@@ -83,267 +87,43 @@ struct PerfilView: View {
 
     // Sección que muestra y gestiona la imagen de perfil
     var profileImageSection: some View {
-        ZStack {
-            Circle()
-                .fill(Color.gray.opacity(0.5))
-                .frame(width: 140, height: 140)
-                .shadow(radius: 10)
-
-            if let image = userData.profileImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFill()
-                    .clipShape(Circle())
-                    .frame(width: 130, height: 130)
-            } else {
-                Image(systemName: "person.circle.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 130, height: 130)
-                    .clipShape(Circle())
-                    .foregroundColor(.white)
-            }
-        }
-        .onTapGesture {
-            self.showImagePicker = true // Activa el selector de imágenes al tocar
-        }
-        .padding(.bottom, 20)
-    }
-
-    // Función para generar campos de usuario editables
-    func userInfoField(label: String, value: Binding<String>, editing: Binding<String?>, fieldKey: String, editable: Bool) -> some View {
-        HStack {
-            if editing.wrappedValue == fieldKey {
-                TextField(label, text: value)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Button(action: { editing.wrappedValue = nil }) {
-                    Image(systemName: "checkmark.circle.fill")
-                        .foregroundColor(.green)
-                }
-            } else {
-                Text(label)
-                Spacer()
-                Text(value.wrappedValue)
-                if editable {
-                    Spacer()
-                    Button(action: { editing.wrappedValue = fieldKey }) {
-                        Image(systemName: "pencil.circle.fill")
-                            .foregroundColor(.blue)
-                    }
-                }
-            }
-        }
-        .padding()
-    }
-
-    // Función para generar un selector de fechas
-    func datePickerField(label: String, date: Binding<Date>, editing: Binding<String?>, fieldKey: String) -> some View {
-        HStack {
-            Text(label)
-            Spacer()
-            if editing.wrappedValue == fieldKey {
-                DatePicker("", selection: date, displayedComponents: [.date])
-                    .labelsHidden()
-                    .datePickerStyle(WheelDatePickerStyle())
-                    .padding(.horizontal)
-                Button(action: { editing.wrappedValue = nil }) {
-                    Image(systemName: "checkmark.circle.fill")
-                        .foregroundColor(.green)
-                }
-            } else {
-                Text("\(date.wrappedValue, formatter: DateFormatter.iso8601Full)")
-                    .padding(.horizontal)
-                Spacer()
-                Button(action: { editing.wrappedValue = fieldKey }) {
-                    Image(systemName: "pencil.circle.fill")
-                        .foregroundColor(.blue)
-                }
-            }
-        }
-        .padding()
-    }
-
-    // Función para guardar los cambios realizados a los datos del usuario
-    func saveData() {
-        // Verifica si hay un usuario actualmente autenticado, si no lo hay, sale de la función
-        guard let userId = Auth.auth().currentUser?.uid else { return }
-
-        // Si hay una imagen de perfil seleccionada, la guarda en Firestore
-        if let profileImage = userData.profileImage {
-            saveProfileImage(userId: userId, image: profileImage)
-        }
-
-        // Obtiene una instancia de Firestore
-        let db = Firestore.firestore()
-
-        // Crea un diccionario con los datos del usuario a guardar en Firestore
-        let userData = [
-            "email": self.userData.email,
-            "firstName": self.userData.firstName,
-            "lastName": self.userData.lastName,
-            "birthDate": DateFormatter.iso8601Full.string(from: self.userData.birthDate),
-            "gender": self.userData.gender,
-            "profileImageURL": self.userData.profileImageURL // Actualiza la URL de la imagen de perfil
-        ]
-
-        // Guarda los datos del usuario en Firestore
-        db.collection("users").document(userId).setData(userData) { error in
-            if let error = error {
-                // Si hay un error al guardar los datos, muestra un mensaje de error
-                print("Error updating user data: \(error.localizedDescription)")
-            } else {
-                // Si los datos se guardan correctamente, muestra un mensaje de éxito
-                print("User data updated successfully")
-                self.showAlert = true
-                self.alertMessage = "Datos guardados correctamente"
-            }
-        }
-    }
-
-    func loadImageFromGoogle() {
-        // Verifica si ya hay una imagen de perfil almacenada en Firestore
-        if userData.profileImage == nil {
-            // Obtiene el usuario actualmente autenticado
-            guard let user = Auth.auth().currentUser else {
-                print("No hay usuario autenticado")
-                return
-            }
-
-            // Obtiene la URL de la imagen de perfil de Google
-            if let imageUrl = user.photoURL {
-                // Descarga la imagen de perfil desde la URL de Google
-                URLSession.shared.dataTask(with: imageUrl) { data, response, error in
-                    guard let data = data, error == nil else {
-                        // Si hay un error al descargar la imagen, muestra un mensaje de error
-                        print("Failed to download image data:", error?.localizedDescription ?? "Unknown error")
-                        return
-                    }
-                    DispatchQueue.main.async {
-                        // Si se descarga la imagen correctamente, la asigna a la imagen de perfil y la guarda en Firestore
-                        if let image = UIImage(data: data) {
-                            self.userData.profileImage = image
-                            self.saveProfileImage(userId: user.uid, image: image)
-                        }
-                    }
-                }.resume()
-            } else {
-                print("No se encontró la URL de la imagen de perfil de Google")
-            }
-        }
-    }
-
-    // Función para cargar los datos del usuario desde Firestore
-    func loadUserData() {
-        // Verifica si hay un usuario actualmente autenticado, si no lo hay, sale de la función
-        guard let userId = Auth.auth().currentUser?.uid else { return }
-
-        // Obtiene una instancia de Firestore
-        let db = Firestore.firestore()
-
-        // Obtiene el documento del usuario actual de la colección "users" en Firestore
-        db.collection("users").document(userId).getDocument { document, error in
-            if let document = document, document.exists {
-                // Si el documento existe, extrae los datos
-                let data = document.data()
-
-                // Actualiza los datos del usuario en la interfaz de usuario en la cola principal
-                DispatchQueue.main.async {
-                    self.userData.email = data?["email"] as? String ?? ""
-                    self.userData.firstName = data?["firstName"] as? String ?? ""
-                    self.userData.lastName = data?["lastName"] as? String ?? ""
-                    self.userData.gender = data?["gender"] as? String ?? ""
-
-                    // Convierte la fecha de nacimiento del formato Timestamp a Date
-                    if let birthDateTimestamp = data?["birthDate"] as? Timestamp {
-                        self.userData.birthDate = birthDateTimestamp.dateValue()
-                    }
-
-                    // Si hay una URL de imagen de perfil en Firestore, la asigna al usuario
-                    if let profileImageURL = data?["profileImageURL"] as? String {
-                        self.userData.profileImageURL = profileImageURL
-
-                        // Carga la imagen desde la URL
-                        self.loadProfileImageFromURL()
-                    } else {
-                        // Si no hay una URL de imagen de perfil en Firestore, carga la imagen desde Google
-                        self.loadImageFromGoogle()
-                    }
-                }
-            } else {
-                // Si el documento no existe, carga la imagen de perfil desde Google
-                self.loadImageFromGoogle()
-            }
-        }
-    }
-
-    // Función para cargar la imagen de perfil desde una URL
-    func loadProfileImageFromURL() {
-        guard let profileImageURL = URL(string: userData.profileImageURL) else {
-            print("Invalid profile image URL")
-            return
-        }
-
-        URLSession.shared.dataTask(with: profileImageURL) { data, response, error in
-            guard let data = data, error == nil else {
-                print("Failed to load profile image:", error?.localizedDescription ?? "Unknown error")
-                return
-            }
-
-            DispatchQueue.main.async {
-                self.userData.profileImage = UIImage(data: data)
-            }
-        }.resume()
-    }
-
-    // Función para guardar la imagen de perfil en Firebase Storage y obtener la URL de descarga
-    func saveProfileImage(userId: String, image: UIImage) {
-        // Redimensiona la imagen para reducir el tamaño de almacenamiento (opcional)
-        let resizedImage = image.resized(to: CGSize(width: 300, height: 300))
-
-        // Convierte la imagen en datos JPEG
-        guard let imageData = resizedImage.jpegData(compressionQuality: 0.5) else {
-            print("Failed to convert image to JPEG data")
-            return
-        }
-
-        // Crea una referencia al directorio del usuario en Firebase Storage
-        let storageRef = Storage.storage().reference().child("profile_images/\(userId).jpg")
-
-        // Sube la imagen al directorio del usuario en Firebase Storage
-        storageRef.putData(imageData, metadata: nil) { metadata, error in
-            guard let _ = metadata, error == nil else {
-                // Si hay un error al subir la imagen, muestra un mensaje de error
-                print("Error uploading profile image:", error?.localizedDescription ?? "Unknown error")
-                return
-            }
-
-            // Obtiene la URL de descarga de la imagen
-            storageRef.downloadURL { url, error in
-                guard let downloadURL = url, error == nil else {
-                    // Si hay un error al obtener la URL de descarga, muestra un mensaje de error
-                    print("Error fetching download URL:", error?.localizedDescription ?? "Unknown error")
-                    return
-                }
-
-                // Actualiza la URL de la imagen de perfil en el objeto de datos del usuario
-                self.userData.profileImageURL = downloadURL.absoluteString
-            }
-        }
+        // ...
     }
 }
 
-struct PerfilView_Previews: PreviewProvider {
-    static var previews: some View {
-        PerfilView()
-    }
-}
+// Vista de representación de un selector de imágenes utilizando UIImagePickerController
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    var sourceType: UIImagePickerController.SourceType // Fuente de la imagen
+    @Environment(\.presentationMode) var presentationMode // Referencia de presentación de la vista
 
-extension DateFormatter {
-    static let iso8601Full: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        return formatter
-    }()
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = sourceType
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            parent.presentationMode.wrappedValue.dismiss() // Cerrar la vista después de seleccionar la imagen
+        }
+    }
 }
 
 // Extensión de UIImage para redimensionar imágenes
@@ -352,6 +132,12 @@ extension UIImage {
         UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
         draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
         return UIGraphicsGetImageFromCurrentImageContext() ?? self
+    }
+}
+
+struct PerfilView_Previews: PreviewProvider {
+    static var previews: some View {
+        PerfilView()
     }
 }
 
